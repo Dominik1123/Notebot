9756e5be266a57f312a7bad01cb04c8dd0dcdf57
Dominik1123 <>
2016-09-05 00:54:50 +0200

    Implement delay calcluation for '@'-format
    Currently the year, month and date have to be given and cannot be omitted.

--- messaging.py
+++ messaging.py
@@ -1,3 +1,4 @@
+import datetime
 from Queue import Queue
 import re
 import time
@@ -6,7 +7,6 @@ import threading
 import sched
 
 import config
-
 
 outgoing_queue = Queue()
 schedule_queue = sched.scheduler(time.time, time.sleep)
@@ -24,6 +24,7 @@ def trigger_by_command(command):
     def wrapper(func):
         setattr(func, 'command', command)
         return func
+
     return wrapper
 
 
@@ -54,6 +55,7 @@ def new_reminder(msg):
     :param msg:
     :return:
     """
+
     def compute_delay(groupdict):
         seconds_per_unit = {
             's': 1,
@@ -63,29 +65,43 @@ def new_reminder(msg):
         }
         if groupdict['mode'] == '+':
             return int(groupdict['delay']) * seconds_per_unit[groupdict['unit']]
+        elif groupdict['mode'] == '@':
+            datetime_string = '%04d-%02d-%02d %02d:%02d:00' % (
+                int(groupdict['year']) or datetime.datetime.now().year,
+                int(groupdict['month']) or datetime.datetime.now().month,
+                int(groupdict['day']) or datetime.datetime.now().day,
+                int(groupdict['hour']) or datetime.datetime.now().hour,
+                int(groupdict['minute']) or datetime.datetime.now().minute
+            )
+            print datetime_string
+            return max(
+                time.mktime(datetime.datetime.strptime(datetime_string, '%Y-%m-%d %H:%M:%S').timetuple()) - time.time(),
+                0)
 
-    date_pattern = '(?P<date>[2-9][0-9][0-9][0-9]-[0-1][0-9]-[0-3][0-9]t|[0-1][0-9]-[0-3][0-9]t|[0-3][0-9]t|t)'
-    time_pattern = '(?P<time>[0-2][0-9]:[0-5][0-9])'
-    when_pattern = '((?P<mode>\+|@)(?P<delay>[0-9]+)(?P<unit>s|m|h|d)|@%s%s)' % (date_pattern, time_pattern)
+    date_pattern = '(?P<year>[2-9][0-9][0-9][0-9])-(?P<month>[0-1][0-9])-(?P<day>[0-3][0-9])\.'
+    time_pattern = '(?P<hour>[0-2][0-9]):(?P<minute>[0-5][0-9])'
+    when_pattern = '(?P<mode>\+|@)((?P<delay>[0-9]+)(?P<unit>s|m|h|d)|%s%s)' % (date_pattern, time_pattern)
     what_pattern = '(?P<what>[a-zA-Z0-9\s]+)'
     pattern = r'%s %s %s' % (new_reminder.command, when_pattern, what_pattern)
     match = re.match(pattern, msg['text'])
     if match is None:
         create_outgoing_error(msg, 'Regex did not match')
         return
+    print match.groupdict()
     outgoing_queue.put((msg['chat']['id'], 'Delay is %s' % compute_delay(match.groupdict())))
 
     def put_reminder_to_outgoing_queue(reminder_text):
-        text = "Hey %s, don't forget to %s" % (msg['from']['first_name'], reminder_text)
+        text = "Hey %s, don't forget to %s!" % (msg['from']['first_name'], reminder_text)
         outgoing_queue.put((msg['chat']['id'], text))
 
     timer = threading.Timer(compute_delay(match.groupdict()), put_reminder_to_outgoing_queue,
                             [match.groupdict()['what']])
     timer.start()
 
+
 command_reactions = {
     getattr(reaction, 'command'): reaction for reaction in [new_reminder]
-}
+    }
 
 error_messages = {
     'command_without_text': 'Cannot extract command from message without text',
