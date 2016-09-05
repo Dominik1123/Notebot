3a96fbd9d3f05f2e5930b71be3b486640d971ce3
Dominik1123 <>
2016-09-05 16:43:39 +0200

    Load config file from /etc/notebot/

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ from pyhocon import ConfigFactory
 
 try:
     # Use a copy of app.conf.template as app.conf and fill in the missing parameters
-    config = ConfigFactory.parse_file('app.conf')
+    config = ConfigFactory.parse_file('/etc/notebot/app.conf')
 except IOError:
-    raise IOError("Use a copy of app.conf.template to create a valid application configuration named 'app.conf' " +
-                  "and fill in the missing parameters")
+    raise IOError("Use a copy of app.conf.template to create a valid application configuration named 'app.conf', " +
+                  "fill in the missing parameters and place it under /etc/notebot/")
